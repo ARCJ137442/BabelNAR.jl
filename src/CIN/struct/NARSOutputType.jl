abd03f8a56a8a3c837e046054df9d50fbc1fc938
ARCJ137442 <61109168+ARCJ137442@users.noreply.github.com>
2024-01-25 15:30:17 +0800

    feat: :sparkles: 0.4.0 新的「口袋输出类型」OTHER
    引入新的「口袋输出类型」OTHER，用于最大化捕获【推理器特异】的信息

--- src/CIN/struct/NARSOutputType.jl
+++ src/CIN/struct/NARSOutputType.jl
@@ -7,15 +7,19 @@ export NARSOutputType
     - # * 无语之@enum宏只支持数值类型
 """
 NARSOutputType = (;
-    IN="IN",
-    OUT="OUT",
-    ERROR="ERROR",
-    ANSWER="ANSWER",
-    ACHIEVED="ACHIEVED",
-    EXE="EXE",
-    INFO="INFO",
-    COMMENT="COMMENT",
-    ANTICIPATE="ANTICIPATE"
+    IN = "IN",
+    OUT = "OUT",
+    ERROR = "ERROR",
+    ANSWER = "ANSWER",
+    ACHIEVED = "ACHIEVED",
+    EXE = "EXE",
+    INFO = "INFO",
+    COMMENT = "COMMENT",
+    ANTICIPATE = "ANTICIPATE",
+    OTHER = "OTHER"
+    # *【2024-01-25 15:27:03】`OTHER`类型用于存储「暂无法格式化识别」的其它信息
+    #     * @example 如OpenNARS的`executed based on`（操作执行的证据基础，用于验证「系统是否习得知识」）
+    #     * 🎯用于在后续实验中提取「推理器特异」的实用信息
 )
 
 NARSOperationVec = String[]
